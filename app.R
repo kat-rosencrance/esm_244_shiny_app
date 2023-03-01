c11b3719a479e9332266ebeb227aabb24f6a0a06
kat-rosencrance <kgr@ucsb.edu>
2023-03-01 00:07:49 -0800

    switched tabs around to make home page, updated locations tab

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(here)
 library(janitor)
 library(readxl)
 library(leaflet)
+library(leaflet.extras)
 library(shinythemes)
 library(shinyWidgets)
 library(plotly)
@@ -12,9 +13,9 @@ library(plotly)
 ### READ IN DATA ###
 
 # map stuff
-usaLat <- 36.5588659
-usaLon <- -107.6660877
-usaZoom <- 3
+usaLat <- 21.196031
+usaLon <- -156.969455
+usaZoom <- 15
 
 # Seal observations
 seal_obs <- read_csv(here("data", "seal_observations.csv")) %>% 
@@ -23,8 +24,8 @@ seal_obs <- read_csv(here("data", "seal_observations.csv")) %>%
                    TRUE ~ sex))
 
 # Genealogy
-# unable to read file probably because it's a gene tree in excel
-# genes <- read_xls(here("data", "genealogy.xlsx"))
+# used the data from the left of the original genealogy sheet. May be able to use this one for the mom and pup widget
+genes <- read_csv(here("data", "genealogy_fix_kr.csv"))
 
 # Pup and mom data
 pup_predict <- read_csv(here("data", "pup_predictions.csv")) %>% 
@@ -36,59 +37,101 @@ pup_data <- read_csv(here("data", "pup_data.csv")) %>%
 
 # view(pup_data)
 # colnames(pup_data)
-
+  
+  
+  
+  
 
 ### CREATE THE USER INTERFACE: ###
 ui <- fluidPage(
   navbarPage(theme = shinytheme("darkly"),
-             title = "Hawaiian Monk Seal",
+             title = "Hawaiian Monk Seals",
+             
+
              
              
-             ### FIRST TAB ###
-           tabPanel("About the Hawaiian Monk Seal", fluid = TRUE, icon = icon("water"),
+             
+             
+             
+             
+                          
+             ###FIRST TAB - HOME PAGE & INTRODUCTION###
+             tabPanel("Home", fluid = TRUE, icon = icon("home"),
+                      mainPanel(h1("Welcome!"),
+                                "Hawaiian Monk Seals is an app that lets you visualize Kalaupapa National Historic Park's monk seal survey data .
+
+From the top navigation panel you'll be able to access graphs displaying seal observation data, observed locations, as well as mother and pup characteristics.
+
+It has been made with shiny for the course ESM 244 Advanced Data Analysis at UCSB.",
+                      HTML('<center><img src="kala_2.jpeg" alt="Mother and offspring monk seal on the beach" style="height: 580px; width:653px;"/></center>'),
+                      )# end mainpanel
+                      ), 
+            ###END FIRST TAB###
+             
+                 
+
+
+
+
                     
+                                     
+                                     
+                              
+                                     
+            ### SECOND TAB - ABOUT MONK SEALS (MAY BE REPLACED WITH GENEALOGY?) ###
+           tabPanel("About Monk Seals", fluid = TRUE, icon = icon("water"),
                     # sidebar layout
                     sidebarLayout(
-                      sidebarPanel("WIDGETS",
+                      sidebarPanel(HTML('<center><img src="kala_park_pic.PNG" alt="Kalaupapa National Historic Park" style="height: 150px; width:200px;"/></center>'),
+                                   hr(),
                                    actionButton("lifecycle", label = "Life Cycle"),
+                                   hr(),
                                    actionButton("pups", label = "Pups"),
+                                   hr(),
                                    actionButton("surveys", label = "Surveys"),
+                                   hr(),
                                    actionButton("genealogy", label = "Genealogy")
                       ), # end sidebar panel
-                      mainPanel("Information about the Monk Seal",
-                                HTML('<center><img src="kala_2.jpeg" alt="Mother and offspring monk seal on the beach" style="height: 580px; width:653px;"/></center>'),
-                                textOutput("value")
+
+                      mainPanel(          
+                      textOutput("value")
                       ) # end mainpanel
                     ) # end sidebar layout
-           ), ### END FIRST TAB ###
+           ), 
+            ### END SECOND TAB ###
            
            
            
+
+
            
            
-           ### SECOND TAB ###
+           ### THIRD TAB - LOCATIONS ###
            tabPanel("Locations",fluid = TRUE, icon = icon("globe-americas"),
            
            # sidebar layout
            sidebarLayout(
-             sidebarPanel("WIDGETS",
+             sidebarPanel(h3("Beaches"),
                           selectInput(inputId = "pick_beach",
                                       label = "Choose a beach:",
                                       choices = unique(seal_obs$beach_location_name_from_standardized_list)
                           )
              ), # end sidebar panel
-             mainPanel("Seal Locations!",
+             mainPanel(h1("Seal Locations!"),
                        leafletOutput(outputId = "beach_map")
              ) # end mainpanel
            ) # end sidebar layout
-  ), ### END SECOND TAB ###
+  ),
+        ### END THIRD TAB ###
   
   
   
+
+
   
   
-  #  ### THIRD TAB ###
-           tabPanel("Seal Characteristics",
+        ### FOURTH TAB - SEAL CHARACTERISTICS ###
+           tabPanel("Oberved Seal Characteristics",
                     HTML('<center><img src="kala_6.jpeg" alt="Mother and offspring monk seal on the beach" style="height: 580px; width:653px;"/></center>'),
                     sidebarLayout(
                       sidebarPanel(
@@ -112,11 +155,15 @@ ui <- fluidPage(
                         plotlyOutput(outputId = "seal_obs_plot")
                       )# end sidebar panel
   ) # end sidebar layout
-  ), # end tab panel
+  ), 
+          ### END FOURTH TAB###
+  
+
+
+
   
   
-  
-  ### FOURTH TAB ###
+         ### FIFTH TAB - MOMS AND PUPS ###
   tabPanel("Moms and Pups",
            
            # sidebar layout
@@ -136,16 +183,20 @@ ui <- fluidPage(
            ) # end sidebar layout
            ) # end tabpanel
   
-  ### END FOURTH TAB###
+      ### END FIFTH TAB###
            ) # end navbarpage 
 
   
 ) # end ui
 
+
+
+
+
 ### Create the server function: ###
 server <- function(input, output) {
 
-  ### FIRST TAB####
+  ### ABOUT SEALS TAB####
   observeEvent(input$lifecycle, {
     output$value <- renderText({"Monk seals spend two-thirds of their life at sea. They’ll molt completely once a year which helps keep their coat clean and free of algae growth. They can live to over 30 years, though life expectancy is often shorter. They can hold their breath for up to 20 minutes and dive more than 1,800 feet! However, an average dive is much shorter and shallower. Though they don’t migrate, they can travel hundreds of miles throughout the Hawaiian archipelago. " })
   })
@@ -159,14 +210,15 @@ server <- function(input, output) {
     output$value <- renderText({"Genealogy info"})
   })
   
- ### SECOND TAB### 
+ ### LOCATION TAB### 
 seal_reactive <- reactive({
   seal_obs %>%
     filter(beach_location_name_from_standardized_list %in% input$pick_beach)
 })
 
 output$beach_map <- renderLeaflet({
-  leaflet(data = seal_obs) %>%
+  leaflet(data = seal_reactive()) %>%
+    addProviderTiles(providers$Thunderforest.Outdoors) %>% 
     setView(lat = usaLat, lng = usaLon, zoom = usaZoom) %>%
     addTiles() %>%
     addMarkers(~x, ~y, popup = ~tag_number, label = ~tag_number)
